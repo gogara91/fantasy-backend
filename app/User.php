346a19214a4ffb8dcea48a5c11da67e1fce15077
Goran Nikolajevic <gorans91@gmail.com>
2019-12-19 02:27:59 +0100

    alter users table

--- app/User.php
+++ app/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable implements JWTSubject
         'email',
         'password'
     ];
+    protected $hidden = ['password'];
     // Rest omitted for brevity
 
     /**

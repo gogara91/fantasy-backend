564af6745bf2175f60be2900ce711ef2bdfd39b4
Goran Nikolajevic <gorans91@gmail.com>
2020-01-30 02:04:13 +0100

    small up

--- database/seeds/TeamPlayerPivotSeeder.php
+++ database/seeds/TeamPlayerPivotSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use App\Team;
 class TeamPlayerPivotSeeder extends Seeder
 {
     /**
@@ -12,11 +12,42 @@ class TeamPlayerPivotSeeder extends Seeder
      */
     public function run()
     {
-        DB::raw(
+        DB::update(
+            "UPDATE team_player_pivot SET
+                contract_signed = NULL,
+                contract_ended = NULL,
+                current_team = NULL"
+        );
+        DB::update(
             "UPDATE team_player_pivot SET
             team_id = (
                 SELECT id FROM teams WHERE team_player_pivot.team_id = teams.api_id
             )"
+        );
+        $teams = Team::all();
+        foreach($teams as $team) {
+            $res = DB::update(
+                "UPDATE team_player_pivot SET
+                contract_signed = NOW() WHERE team_id = {$team->id} ORDER BY RAND() LIMIT 15"
+            );
+        }
+
+        DB::update(
+            "UPDATE team_player_pivot SET
+            contract_ended = NOW() WHERE contract_signed IS NULL"
+        );
+
+        DB::update(
+            "UPDATE team_player_pivot SET
+            contract_signed = NOW() WHERE contract_signed IS NOT NULL"
+        );
+
+        DB::update(
+            "UPDATE team_player_pivot SET current_team = 0 WHERE contract_ended IS NOT NULL"
+        );
+
+        DB::update(
+            "UPDATE team_player_pivot SET current_team = 1 WHERE contract_ended IS NULL"
         );
     }
 }

f0f06d90885e28925ac5e3d5b00e16da82d6476d
Goran Nikolajevic <gorans91@gmail.com>
2020-05-13 15:28:36 +0200

    fixed seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TeamsSeeder::class);
         $this->call(PlayersSeeder::class);
         $this->call(TeamPlayerPivotSeeder::class);
-        $this->call(TeamPlayerPivotSeeder::class);
         $this->call(ConferencesSeeder::class);
         $this->call(DivisionsSeeder::class);
         $this->call(SeasonSeeder::class);
